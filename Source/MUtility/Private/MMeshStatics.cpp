ab13b5d6200e3dbb988d86563c2f05bd81ce1b9e
Mikolaj Wojcik <miknios@gmail.com>
2025-06-27 00:54:26 +0200

    Added MMeshStatics with some functions

---
+++ Source/MUtility/Private/MMeshStatics.cpp
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Miknios. All rights reserved.
+
+
+#include "MMeshStatics.h"
+
+void UMMeshStatics::AttachComponentsSocketAligned(USceneComponent* ChildComponent, const FName ChildSocket,
+                                                  USceneComponent* ParentComponent, const FName ParentSocket)
+{
+	ChildComponent->AttachToComponent(
+		ParentComponent,
+		FAttachmentTransformRules::SnapToTargetNotIncludingScale,
+		ParentSocket);
+
+	const FTransform ChildSocketLocalTransform = ChildComponent->GetSocketTransform(ChildSocket, RTS_Component);
+	const FTransform InverseChildSocketLocalTransform = ChildSocketLocalTransform.Inverse();
+	ChildComponent->SetRelativeTransform(InverseChildSocketLocalTransform);
+}
---
+++ Source/MUtility/Public/MMeshStatics.h
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Miknios. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MMeshStatics.generated.h"
+
+UCLASS()
+class MUTILITY_API UMMeshStatics : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	static void AttachComponentsSocketAligned(USceneComponent* ChildComponent, FName ChildSocket,
+	                                          USceneComponent* ParentComponent, FName ParentSocket);
+};
