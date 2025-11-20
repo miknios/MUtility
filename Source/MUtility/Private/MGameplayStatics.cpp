dfc92e40cbe280aba7b017c7f14027ca2ccf2da8
Mikolaj Wojcik <miknios@gmail.com>
2025-11-20 23:18:43 +0100

    Missing GetDynamicOverlayMaterialEnsure implementation in .cpp

--- Source/MUtility/Private/MGameplayStatics.cpp
+++ Source/MUtility/Private/MGameplayStatics.cpp
@@ -10,3 +10,26 @@ AActor* UMGameplayStatics::StaticSpawnActorFromClass(UObject* WorldContextObject
 
 	return SpawnedActor;
 }
+
+UMaterialInstanceDynamic* UMGameplayStatics::GetDynamicOverlayMaterialEnsure(UMeshComponent* MeshComponent)
+{
+	if (MeshComponent == nullptr)
+	{
+		return nullptr;
+	}
+	
+	UMaterialInterface* MaterialInterface = MeshComponent->GetOverlayMaterial();
+	if (MaterialInterface == nullptr)
+	{
+		return nullptr;
+	}
+
+	UMaterialInstanceDynamic* MaterialInstanceDynamic = Cast<UMaterialInstanceDynamic>(MaterialInterface);
+	if (MaterialInstanceDynamic == nullptr)
+	{
+		MaterialInstanceDynamic = UMaterialInstanceDynamic::Create(MaterialInterface, MeshComponent);
+		MeshComponent->SetOverlayMaterial(MaterialInstanceDynamic);
+	}
+
+	return MaterialInstanceDynamic;
+}
