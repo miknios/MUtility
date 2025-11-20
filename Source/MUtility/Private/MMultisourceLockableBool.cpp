67ba5f65f369c8bb267460c0b786073066f34c3b
Mikolaj Wojcik <miknios@gmail.com>
2025-11-20 23:18:21 +0100

    Missing .cpp for MMultisourceLockableBool

---
+++ Source/MUtility/Private/MMultisourceLockableBool.cpp
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Miknios. All rights reserved.
+
+
+#include "MMultisourceLockableBool.h"
+
+bool UMMultisourceLockableBool::IsActive() const
+{
+	return BlockSources.IsEmpty();
+}
+
+void UMMultisourceLockableBool::AddBlockSource(const UObject* BlockSource)
+{
+	const bool bActiveOld = IsActive();
+	
+	BlockSources.Emplace(BlockSource);
+	const bool bActiveNew = IsActive();
+
+	if (bActiveOld != bActiveNew)
+	{
+		OnValueChangedDelegate.Broadcast(bActiveNew);
+	}
+}
+
+void UMMultisourceLockableBool::RemoveBlockSource(const UObject* BlockSource)
+{
+	const bool bActiveOld = IsActive();
+	
+	BlockSources.Remove(BlockSource);
+	const bool bActiveNew = IsActive();
+
+	if (bActiveOld != bActiveNew)
+	{
+		OnValueChangedDelegate.Broadcast(bActiveNew);
+	}
+}
